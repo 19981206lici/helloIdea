2f5da29b69c2b8a6426a7a2eba0f0d575e536c02
Lc666 <863425651@qq.com>
2020-05-25 10:29:10 +0800

    添加文件

---
+++ src/main/java/cn/itcast/dao/company/CompanyDao.java
@@ -0,0 +1,32 @@
+package cn.itcast.dao.company;
+
+import cn.itcast.domain.company.Company;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author shkstart
+ * @create 2020-05-08 13:03
+ * dao层接口
+ */
+@Repository
+public interface CompanyDao {
+
+    /**
+     * 查询所有的企业列表
+     */
+    List<Company> findAll();
+
+
+    //企业保存
+    void save(Company company);
+
+    Company findById(String id);
+
+    //更新
+    void update(Company company);
+
+    void delete(String id);
+}
